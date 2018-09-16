cafd258ed6f9b68156294fabdf3b8d638b65affd
vinceajcs <hvincentstephen@gmail.com>
2018-09-15 17:11:33 -0700

    add xhr template

--- ajax/boilerplate.js
+++ ajax/boilerplate.js
@@ -1,5 +1,20 @@
-//fetch GET request boilerplate
+//XMLHttpRequest GET boilerplate
+const xhr = new XMLHttpRequest();
+const url = "https://api-to-call.com/endpoint";
 
+xhr.responseType = 'json';
+xhr.onreadystatechange = () => {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    return xhr.response;
+  }
+};
+
+//open request and send object
+xhr.open('GET', url);
+xhr.send();
+
+
+//fetch GET boilerplate
 //send request
 fetch("https://api-to-call.com/endpoint").then((response) => {
   //convert response object to JSON
